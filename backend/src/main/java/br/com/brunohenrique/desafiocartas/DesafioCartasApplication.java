de7576e769c407be36e3249bc32ca3436d523fe6
Bruno Henrique Silva Contente <brunohenriquecontente@hotmail.com>
2023-10-21 18:13:06 -0300

    First Commit
    first commit with basic structure

---
+++ backend/src/main/java/br/com/brunohenrique/desafiocartas/DesafioCartasApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunohenrique.desafiocartas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCartasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCartasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/brunohenrique/desafiocartas/DesafioCartasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunohenrique.desafiocartas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCartasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
