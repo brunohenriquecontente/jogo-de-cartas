02825d361b5a1231ad4efdb466c730523c7446bf
Bruno Henrique Silva Contente <brunohenriquecontente@hotmail.com>
2023-10-23 12:56:01 -0300

    Improved DTO's

--- backend/src/main/java/br/com/brunohenrique/desafiocartas/dto/CardDTO.java
+++ backend/src/main/java/br/com/brunohenrique/desafiocartas/dto/CardDTO.java
@@ -1,6 +1,7 @@
 package br.com.brunohenrique.desafiocartas.dto;
 
 public record CardDTO(
+        Long id,
         String code,
         String value,
         String suit
--- backend/src/main/java/br/com/brunohenrique/desafiocartas/dto/PlayerDTO.java
+++ backend/src/main/java/br/com/brunohenrique/desafiocartas/dto/PlayerDTO.java
@@ -1,10 +1,9 @@
 package br.com.brunohenrique.desafiocartas.dto;
 
-import br.com.brunohenrique.desafiocartas.entity.CardEntity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.util.List;
 
-public record PlayerDTO(Long id, String name, @JsonIgnore List<CardEntity> cards) {
+public record PlayerDTO(Long id, String name, @JsonIgnore List<CardDTO> cards, @JsonIgnore MatchDTO matchDTO) {
 
 }
